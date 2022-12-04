efd8122cc12c3d72733316af36d7d57a75282c64
Habib Ur Rahman <hab67603@gmail.com>
2022-12-04 21:48:57 +0500

    add new directory

---
+++ hello/src/components/Service.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Service extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Services</h1>
+      </div>
+    )
+  }
+}

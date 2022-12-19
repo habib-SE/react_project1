4a9649c10357c3cbfbfc2b58d10b66899a27b981
Habib Ur Rahman <hab67603@gmail.com>
2022-12-19 17:10:01 +0500

    add new file of common and make othe updation

--- hello/src/components/Home.js
+++ hello/src/components/Home.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
-
-export default class Home extends Component {
-  render() {
-    return (
-      <div>
-       
-      </div>
-    )
-  }
-}
+import React from "react";
+import web from "../components/image/pexels-caleb-oquendo-4889278.jpg";
+import Common from "./Common";
+const Home = () => {
+  return (
+    <>
+      <Common
+        name=" Grow your busniess with"
+        imgsrc={web}
+        visit="/service"
+        btname="Get started"
+      />
+    </>
+  );
+};
+export default Home;
